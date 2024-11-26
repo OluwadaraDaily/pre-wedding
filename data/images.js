a7479e7f63b61cd054b03617421dd975e3162509
Oluwadara Oloye <daraoloye99@gmail.com>
2024-11-26 19:42:32 +0100

    style: update ui for homepage with pictures

--- data/images.js
+++ data/images.js
@@ -1,2 +1,4 @@
 export const BACKGROUND_IMAGE = "https://res.cloudinary.com/oluwadaradaily/image/upload/v1731018352/wedding-page/PXL_20240225_173249138.PORTRAIT_i2nnxl.jpg";
 
+export const BACKGROUND_IMAGE_2 = "https://res.cloudinary.com/oluwadaradaily/image/upload/v1731018361/wedding-page/PXL_20231118_141208668.MP_wdvuxg.jpg";
+
